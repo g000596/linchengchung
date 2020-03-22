c253fb95c9caa29f586f4374bcc517a69e77f52b
g000596 <g000596@gmail.com>
2020-03-22 20:32:15 +0800

    no message

---
+++ 20200130/js/lol.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//     $(".close,.ppb").click(function(){
+//         $(".popup-bg").hide();
+//     });
+//     $("#btn-3").click(function(){
+//         $(".popup-bg").hide();
+//         $("#register").show();
+//     });
+//     $("#btn1").click(function(){
+//         $("#register").show();
+      
+//     });
+//     $("#btn2").click(function(){
+//         $("#login").show();
+      
+//     });
+// });
+
+$(document).ready(function(){
+	$('#nav-icon1').click(function(){
+        $(this,).toggleClass('open'); 
+           $('.nav-open').toggleClass('open');
+	});
+});
