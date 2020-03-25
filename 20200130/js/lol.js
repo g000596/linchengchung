c4acce48540219e7f265efe51a7c520ca32df6f7
g000596 <g000596@gmail.com>
2020-03-25 09:55:09 +0800

    no message

--- 20200130/js/lol.js
+++ 20200130/js/lol.js
@@ -15,11 +15,49 @@
       
 //     });
 // });
-
+        // $(".t").html("<p>多能力女實況主，主持、cosplay、玩遊戲樣樣行，反差萌月希正在狙擊你的心！</p>");
+        // $(".").css("color","red");
+        // $(this,).toggleClass('open'); 
+        //    $('.j1m').toggleClass('open');
+        //    $('html').toggleClass('m-off');
+        $(document).ready(function () {
+            $('#nav-icon1').click(function () {
+              $(this, ).toggleClass('open');
+              $('.nav-open').toggleClass('open');
+              $('html').toggleClass('m-off');
+            });
+          });
 $(document).ready(function(){
-	$('#nav-icon1').click(function(){
-        $(this,).toggleClass('open'); 
-           $('.nav-open').toggleClass('open');
-           $('html').toggleClass('m-off');
-	});
+    $('#navj1').hover(function(){
+        $(".live-box").hide();
+        $("#j1m").show();
+    });
+    $('#navj2').hover(function(){
+        $(".live-box").hide();
+        $("#j2m").show();
+    });
+    $('#navj3').hover(function(){
+        $(".live-box").hide();
+        $("#j3m").show();
+    });
+	$('#navj4').hover(function(){
+        $(".live-box").hide();
+        $("#j4m").show();
+    });
+    $('#navy1').hover(function(){
+        $(".live-box").hide();
+        $("#y1m").show();
+    });
+    $('#navy2').hover(function(){
+        $(".live-box").hide();
+        $("#y2m").show();
+    });
+    $('#navy3').hover(function(){
+        $(".live-box").hide();
+        $("#y3m").show();
+    });
+    $('#navy4').hover(function(){
+        $(".live-box").hide();
+        $("#y4m").show();
+    });
 });
