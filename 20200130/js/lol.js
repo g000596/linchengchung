a1eadfee9f3dae0813ebaf8f9f706de2e2893f70
g000596 <g000596@gmail.com>
2020-03-26 01:42:56 +0800

    no message

--- 20200130/js/lol.js
+++ 20200130/js/lol.js
@@ -1,30 +1,16 @@
-// $(document).ready(function(){
-//     $(".close,.ppb").click(function(){
-//         $(".popup-bg").hide();
-//     });
-//     $("#btn-3").click(function(){
-//         $(".popup-bg").hide();
-//         $("#register").show();
-//     });
-//     $("#btn1").click(function(){
-//         $("#register").show();
-      
-//     });
-//     $("#btn2").click(function(){
-//         $("#login").show();
-      
-//     });
-// });
-        // $(".t").html("<p>多能力女實況主，主持、cosplay、玩遊戲樣樣行，反差萌月希正在狙擊你的心！</p>");
-        // $(".").css("color","red");
-        // $(this,).toggleClass('open'); 
-        //    $('.j1m').toggleClass('open');
-        //    $('html').toggleClass('m-off');
+
         $(document).ready(function () {
-            $('#nav-icon1,.tr').click(function () {
+            $('#nav-icon1').click(function () {
               $(this, ).toggleClass('open');
               $('.nav-open').toggleClass('open');
               $('html').toggleClass('m-off');
+            });
+
+          });
+          $(document).ready(function () {
+            $('.tr').click(function () {
+                $('.open').toggleClass('open');
+             
             });
           });
           $(document).ready(function () {
@@ -95,3 +81,33 @@ $('.btn-a4').click(function () {
         scrollTop: $('#ap4').offset().top
     }, 800);
 });
+
+$(document).ready(function () {
+    $('#close,#closee').click(function () {
+        $(".popup").fadeOut(1000);
+    });
+    $('.j1btn').click(function () {
+        $("#j1mp").fadeIn(100);
+    });
+    $('.j2btn').click(function () {
+        $("#j2mp").fadeIn(100);
+    });
+    $('.j3btn').click(function () {
+        $("#j3mp").fadeIn(100);
+    });
+    $('.j4btn').click(function () {
+        $("#j4mp").fadeIn(100);
+    });
+    $('.y1btn').click(function () {
+        $("#y1mp").fadeIn(100);
+    });
+    $('.y2btn').click(function () {
+        $("#y2mp").fadeIn(100);
+    });
+    $('.y3btn').click(function () {
+        $("#y3mp").fadeIn(100);
+    });
+    $('.y4btn').click(function () {
+        $("#y4mp").fadeIn(100);
+    });
+  });
